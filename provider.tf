f826c475022f3ec8682d6412fda46f6ac91a8277
Bojan Djokic <bojandjokic974@gmail.com>
2025-07-09 09:45:02 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,16 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "yourtfstatestorage"
+    storage_account_name = "yourtfstatestorage"  # Change this to your unique storage account name
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
